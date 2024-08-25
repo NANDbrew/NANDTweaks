e5397ae40266c6a1387a0965f0641c54fbd65d13
NANDbrew <zortman@gmail.com>
2024-08-25 14:55:33 -0500

    new patch: adjust map held distance when rolled/unrolled

---
+++ Patches/ShipItemFoldablePatches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NANDTweaks.Patches
+{
+    internal class ShipItemFoldablePatches
+    {
+        [HarmonyPatch(typeof(ShipItemFoldable))]
+        private static class FoldUnfoldPatch
+        {
+            [HarmonyPatch("Fold")]
+            [HarmonyPostfix]
+            public static void FoldPatch(ref float ___holdDistance)
+            {
+                ___holdDistance = 0.8f;
+
+            }
+
+            [HarmonyPatch("Unfold")]
+            [HarmonyPostfix]
+            public static void UnfoldPatch(ref float ___holdDistance)
+            {
+                ___holdDistance = 0.5f;
+
+            }
+        
+        }
+    }
+}
