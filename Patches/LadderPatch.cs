03b85b7411541fe3893c47717ed09df2caec52f6
NANDbrew <zortman@gmail.com>
2024-10-23 23:41:27 -0500

    New feature: animated ladder climbing, player can now use ladders from nearby boats

---
+++ Patches/LadderPatch.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using SailwindModdingHelper;
+using System.Collections;
+
+namespace NANDTweaks.Patches
+{
+    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "Update")]
+    internal static class PlayerEmbarkPatch
+    {
+        public static bool Prefix()
+        {
+            if (Plugin.ladderPatch.Value && LadderPatch.animating) return false;
+            return true;
+
+        }
+
+    }
+    [HarmonyPatch(typeof(BoatLadder), "OnActivate")]
+    internal static class LadderPatch
+    {
+        public static bool animating;
+        public static bool Prefix(BoatLadder __instance)
+        {
+            if (!Plugin.ladderPatch.Value) return true;
+            float forwardNum = Plugin.embarkDist.Value;;
+            if (__instance.transform.localPosition.z > 0) forwardNum = -forwardNum;
+            Vector3 offset = new Vector3(0, 1.25f, forwardNum);
+
+            PlayerEmbarkDisembarkTrigger player = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerEmbarkDisembarkTrigger>();
+            Transform transform = player.playerController.transform;
+            BoatEmbarkCollider embarkCol = __instance.GetComponentInParent<BoatDamage>().GetComponentInChildren<BoatEmbarkCollider>();
+
+            if (transform.parent == embarkCol.walkCollider) return false;
+            if (PlayerEmbarkDisembarkTrigger.embarked)
+            {
+                player.InvokePrivateMethod("ExitBoat");
+            }
+            player.InvokePrivateMethod("EnterBoat", new object[] { embarkCol.transform.parent, embarkCol.walkCollider } );
+            __instance.StartCoroutine(Scripts.LerpMovement.HackPlayerPosLocal(transform, __instance.transform, offset, 4f));
+            return false;
+        }
+    }
+}
---
+++ Scripts/LerpMovement.cs
@@ -0,0 +1,40 @@
+﻿using NANDTweaks.Patches;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NANDTweaks.Scripts
+{
+    public static class LerpMovement
+    {
+        public static IEnumerator HackPlayerPos(Transform player, Transform target, Vector3 targetOffset, float lerpSpeed)
+        {
+            LadderPatch.animating = true;
+            Vector3 start = player.position;
+            for (float t = 0f; t < 1f; t += Time.deltaTime * lerpSpeed)
+            {
+                player.position = Vector3.Lerp(start, target.position + targetOffset, t);
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("Player ending position = " + target.position);
+            LadderPatch.animating = false;
+        }
+        public static IEnumerator HackPlayerPosLocal(Transform player, Transform target, Vector3 targetOffset, float lerpSpeed)
+        {
+            LadderPatch.animating = true;
+            Vector3 start = player.localPosition;
+            for (float t = 0f; t < 1f; t += Time.deltaTime * lerpSpeed)
+            {
+                player.localPosition = Vector3.Lerp(start, target.localPosition + targetOffset, t);
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("Player ending position = " + target.localPosition);
+            LadderPatch.animating = false;
+        }
+
+    }
+}
