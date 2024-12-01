9f0585e0d2e506c0c1806a3a0a9e94f6034f389c
NANDbrew <zortman@gmail.com>
2024-12-01 15:20:55 -0600

    extra setting for skip disclaimer: keep screen black until physics settles

--- Patches/Fpatcher.cs
+++ Patches/Fpatcher.cs
@@ -1,22 +1,60 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace NANDTweaks.Patches
 {
-    [HarmonyPatch(typeof(StartMenu), "Start")]
+    [HarmonyPatch(typeof(StartMenu))]
     internal static class FPatcher
     {
         [HarmonyPostfix]
-        public static void UpdatePatch(ref bool ___fPressed)
+        [HarmonyPatch("LoadGame")]
+        public static void LoadPatch(StartMenu __instance, ref bool ___fPressed)
         {
-            if (!Plugin.skipDisclaimer.Value) return;
-            ___fPressed = true;
-
+            if (Plugin.skipDisclaimer.Value)
+            {
+                ___fPressed = true;
+            }
+            if (Plugin.hideLoading.Value)
+            {
+                __instance.StartCoroutine(FPatchWaiter(__instance));
+            }
         }
-    }
 
+        public static IEnumerator FPatchWaiter(MonoBehaviour source)
+        {
+            OVRScreenFade fade = Camera.main.GetComponent<OVRScreenFade>();
+            yield return new WaitForSeconds(1);
+            fade.SetFadeLevel(1f);
+ /*           while (GameState.currentlyLoading)
+            {
+                yield return new WaitForEndOfFrame();
+            }*/
+
+            Debug.Log("waiting for trigger");
+            yield return new WaitUntil(() => GameState.justStarted);
+            while (GameState.justStarted)
+            {
+                fade.SetFadeLevel(1f);
+                if (Sleep.instance.recoveryText.text.Length == 0) Sleep.instance.recoveryText.text = "\n\n\nsettling...";
+                yield return new WaitForEndOfFrame();
+            }
+            Sleep.instance.recoveryText.text = "";
+            for (float t = 0f; t < 1f; t += Time.deltaTime)
+            {
+                float fadeLevel = Mathf.Lerp(1f, 0, t / 1f);
+                fade.SetFadeLevel(fadeLevel);
+                yield return new WaitForEndOfFrame();
+            }
+
+            fade.SetFadeLevel(0f);
+            Debug.Log("did the wazooo");
+        }
+
+    }
 }
