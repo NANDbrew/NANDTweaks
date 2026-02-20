205df412cb62d5d0660699c9f7fff4b6484e0f2e
NANDbrew <zortman@gmail.com>
2026-02-20 15:13:50 -0600

    temporarily removed Ladder Tweaks for compatibility with Sailwind 0.35

--- Patches/LadderPatch.cs
+++ Patches/LadderPatch.cs
@@ -1,58 +1,57 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
-using System.Collections;
+﻿//using HarmonyLib;
+//using UnityEngine;
 
-namespace NANDTweaks.Patches
-{
-    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "Update")]
-    internal static class PlayerEmbarkPatch
-    {
-        public static bool Prefix(bool __runOriginal)
-        {
-            if (Plugin.ladderPatch.Value && (LadderPatch.animating || !__runOriginal)) return false;
-            return true;
+//namespace NANDTweaks.Patches
+//{
+///*    [HarmonyPatch(typeof(PlayerEmbarkDisembarkTrigger), "LateUpdate")]
+//    internal static class PlayerEmbarkPatch
+//    {
+//        public static bool Prefix(bool __runOriginal)
+//        {
+//            if (Plugin.ladderPatch.Value && (LadderPatch.animating || !__runOriginal)) return false;
+//            return true;
 
-        }
+//        }
 
-    }
-    [HarmonyPatch(typeof(BoatLadder), "OnActivate")]
-    internal static class LadderPatch
-    {
-        public static bool animating;
-        public static bool Prefix(BoatLadder __instance, float ___upDistance)
-        {
-            if (!Plugin.ladderPatch.Value) return true;
+//    }*/
+//    [HarmonyPatch(typeof(BoatLadder), "OnActivate")]
+//    internal static class LadderPatch
+//    {
+//        public static bool animating;
+//        public static bool Prefix(BoatLadder __instance)
+//        {
+//            if (!Plugin.ladderPatch.Value) return true;
 
-            PlayerEmbarkDisembarkTrigger player = Refs.observerMirror.GetComponentInChildren<PlayerEmbarkDisembarkTrigger>();
-            Transform transform = player.playerController.transform;
-            BoatRefs boatRefs = __instance.GetComponentInParent<BoatRefs>();
+//            PlayerEmbarkerNew player = Refs.observerMirror.GetComponentInChildren<PlayerEmbarkDisembarkTrigger>();
+//            if (player == null)
+//            {
+//                Debug.Log("ladder tweaks: player not found");
+//                return true;
+//            }
+//            Transform transform = player.playerController.transform;
+//            BoatRefs boatRefs = __instance.GetComponentInParent<BoatRefs>();
 
-            if (transform.parent == boatRefs.walkCol) return false;
+//            if (transform.parent == boatRefs.walkCol) return false;
 
-            //float forwardNum = Plugin.embarkDist.Value;
-            //if (__instance.transform.localPosition.z > 0) forwardNum = -forwardNum;
-            //Vector3 offset = new Vector3(0, ___upDistance, forwardNum);
+//            //float forwardNum = Plugin.embarkDist.Value;
+//            //if (__instance.transform.localPosition.z > 0) forwardNum = -forwardNum;
+//            //Vector3 offset = new Vector3(0, ___upDistance, forwardNum);
 
-            float forwardNum = Vector3.Dot(__instance.transform.up, boatRefs.boatModel.transform.position - __instance.transform.position) < 0? -Plugin.embarkDist.Value : Plugin.embarkDist.Value;//__instance.transform.forward;
-            //Plugin.logSource.Log(BepInEx.Logging.LogLevel.Debug, "forward num = " + forwardNum);
+//            float forwardNum = Vector3.Dot(__instance.transform.up, boatRefs.boatModel.transform.position - __instance.transform.position) < 0? -Plugin.embarkDist.Value : Plugin.embarkDist.Value;//__instance.transform.forward;
+//            //Plugin.logSource.Log(BepInEx.Logging.LogLevel.Debug, "forward num = " + forwardNum);
             
-            Vector3 vector = __instance.transform.position + Vector3.up * Plugin.embarkHeight.Value + __instance.transform.up * forwardNum;
-            Vector3 targetPos = boatRefs.boatModel.InverseTransformPoint(vector);
+//            Vector3 vector = __instance.transform.position + Vector3.up * Plugin.embarkHeight.Value + __instance.transform.up * forwardNum;
+//            Vector3 targetPos = boatRefs.boatModel.InverseTransformPoint(vector);
 
-            if (PlayerEmbarkDisembarkTrigger.embarked)
-            {
-                AccessTools.Method(player.GetType(), "ExitBoat").Invoke(player, null);
-            }
-            AccessTools.Method(player.GetType(), "EnterBoat").Invoke(player, new object[] { boatRefs.boatModel, boatRefs.walkCol } );
-            Refs.charController.enabled = false;
-            __instance.StartCoroutine(Scripts.LerpMovement.HackPlayerPosLocal(transform, targetPos, 4f));
-            Refs.charController.enabled = true;
-            return false;
-        }
-    }
-}
+//            if (PlayerEmbarkDisembarkTrigger.embarked)
+//            {
+//                AccessTools.Method(player.GetType(), "ExitBoat").Invoke(player, null);
+//            }
+//            AccessTools.Method(player.GetType(), "EnterBoat").Invoke(player, new object[] { boatRefs.boatModel, boatRefs.walkCol } );
+//            Refs.charController.enabled = false;
+//            __instance.StartCoroutine(Scripts.LerpMovement.HackPlayerPosLocal(transform, targetPos, 4f));
+//            Refs.charController.enabled = true;
+//            return false;
+//        }
+//    }
+//}
--- Scripts/LerpMovement.cs
+++ Scripts/LerpMovement.cs
@@ -1,10 +1,5 @@
 ﻿using NANDTweaks.Patches;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace NANDTweaks.Scripts
@@ -13,7 +8,7 @@ namespace NANDTweaks.Scripts
     {
         public static IEnumerator HackPlayerPos(Transform player, Transform target, Vector3 targetOffset, float lerpSpeed)
         {
-            LadderPatch.animating = true;
+            //LadderPatch.animating = true;
             Vector3 start = player.position;
             for (float t = 0f; t < 1f; t += Time.deltaTime * lerpSpeed)
             {
@@ -21,11 +16,11 @@ namespace NANDTweaks.Scripts
                 yield return new WaitForEndOfFrame();
             }
             Plugin.logSource.Log(BepInEx.Logging.LogLevel.Debug, "Player ending position = " + target.position);
-            LadderPatch.animating = false;
+            //LadderPatch.animating = false;
         }
         public static IEnumerator HackPlayerPosLocal(Transform player, Vector3 targetOffset, float lerpSpeed)
         {
-            LadderPatch.animating = true;
+            //LadderPatch.animating = true;
             Vector3 start = player.localPosition;
             for (float t = 0f; t < 1f; t += Time.deltaTime * lerpSpeed)
             {
@@ -33,7 +28,7 @@ namespace NANDTweaks.Scripts
                 yield return new WaitForEndOfFrame();
             }
             Plugin.logSource.Log(BepInEx.Logging.LogLevel.Debug, "Player ending position = " + player.localPosition);
-            LadderPatch.animating = false;
+            //LadderPatch.animating = false;
         }
 
     }
