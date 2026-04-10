1c86fa9930972088bbdf19538dc77b47e363b755
NANDbrew <zortman@gmail.com>
2026-04-10 11:20:26 -0500

    patch keybind button to unbind key on backspace

---
+++ Patches/KeyBindPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NANDTweaks.Patches
+{
+    [HarmonyPatch(typeof(GPButtonKeybinding), "SetNewKey")]
+    public static class KeyBindPatch
+    {
+        private static void Postfix(KeyCode newKey, InputName ___inputNameEnum, bool ___input2, bool ___controllerButton)
+        {
+            if (newKey == KeyCode.Backspace)
+            {
+                GameInput.SetKeyMap(___inputNameEnum, KeyCode.None, ___input2, ___controllerButton);
+
+            }
+
+        }
+    }
+}
